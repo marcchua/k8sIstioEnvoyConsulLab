3b9d8b0d80b7f10189fb44311bab3e831d30e3ed
Marc Chua <marc@hashicorp.com>
2020-05-12 16:52:31 -0400

    added new node pool

---
+++ k8s/node_pool.tf
@@ -0,0 +1,22 @@
+/*
+resource "google_container_node_pool" "k8s_node_pool" {
+  name       = "${var.cluster_name}-node_pool"
+  location   = var.zone
+  cluster    = google_container_cluster.k8s.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n2-standard-2"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+*/
